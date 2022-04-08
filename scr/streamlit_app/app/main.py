0deab8959cc6e056a3a836a32b678b81af9208c3
Esteban <60893597+Ezuniga13@users.noreply.github.com>
2022-04-08 13:23:39 -0400

    added 3d map to streamlit app

--- scr/streamlit_app/app/main.py
+++ scr/streamlit_app/app/main.py
@@ -1,3 +1,4 @@
+from urllib import request
 import streamlit as st
 import pandas as pd
 from dotenv import load_dotenv 
@@ -5,12 +6,14 @@ from dotenv import find_dotenv
 import os
 import boto3
 import io
+import pydeck as pdk
+import numpy as np
 
 
 
 st.title(''' SIMPLY THE BEST EATS NYC ''')
 
-
+@st.cache(persist=True)
 def get_csv_from_bucket(ACCESS_ID, ACCESS_KEY):
     s3_client = boto3.client('s3',
          aws_access_key_id=ACCESS_ID,
@@ -24,23 +27,54 @@ def make_dataframe(csv):
     yelp_df = pd.read_csv(io.BytesIO(csv))
     yelp_df.index  = yelp_df.index + 1
     yelp_df = yelp_df.loc[:, ~yelp_df.columns.str.contains('Unnamed')]
+    yelp_df = yelp_df.loc[:, ~yelp_df.columns.str.contains('phone')]
     return yelp_df
 
+
+load_dotenv(dotenv_path=find_dotenv(), verbose=True)
+ACCESS_ID = os.getenv('ACCESS_ID')
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+csv = get_csv_from_bucket(ACCESS_ID, ACCESS_KEY)
+df = make_dataframe(csv)
+
+
+st.subheader('Top Eats with a 4.5 or higher Yelp rating!') 
+if st.checkbox("Click to view List", False): 
+    st.dataframe(df.style.format(subset=['rating'], formatter='{:.1f}'))
     
+st.header('3D Map')
+
+if st.checkbox('Click to view map'):
+    midpoint = [np.average(df['latitude']), np.average(df['longitude'])]
+
+    st.pydeck_chart(pdk.Deck(
+                map_style="mapbox://styles/mpbox/light-v10",
+                initial_view_state= pdk.ViewState(
+                                        latitude=midpoint[0],
+                                        longitude=midpoint[1],
+                                        zoom=11,
+                                        pitch=50,
+                ),
+                
+                layers=[
+                    pdk.Layer(
+                        'HexagonLayer',
+                        data=df,
+                        get_position='[longitude, latitude]',
+                        radius=200,
+                        elevation_scale=4,
+                        elevation_range=[0, 1000],
+                        pickable=True,
+                        extruded=True,
+                            )
+                        ]
+                    ))
+    
+
+
 
 def main():
-    load_dotenv(dotenv_path=find_dotenv(), verbose=True)
-    ACCESS_ID = os.getenv('ACCESS_ID')
-    ACCESS_KEY = os.getenv('ACCESS_KEY')
+    pass
     
-    csv = get_csv_from_bucket(ACCESS_ID, ACCESS_KEY)
-    df = make_dataframe(csv)
-    
-    st.dataframe(df.style.format(subset=['rating'], formatter='{:.1f}'))
-    st.map(df)
-    
-
-
-
 if __name__ == '__main__':
     main()
