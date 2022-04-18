23533bf9ba1a68a094c1b2c510ae72e4c5e3d28d
Esteban Zuniga <60893597+Ezuniga13@users.noreply.github.com>
2022-04-18 16:30:29 -0400

    Update main.py

--- scr/yelp_functions/query/query_to_s3/main.py
+++ scr/yelp_functions/query/query_to_s3/main.py
@@ -6,9 +6,6 @@ from dotenv import find_dotenv
 import os
 import boto3
 
-
-
-
 def connect_to_db(host, dbname, username, password, port):
     try:
         conn =  ps.connect(host = host, database = dbname,  user = username,  password = password, port = port)
@@ -37,7 +34,6 @@ def make_dataframe(queryresults):
     yelp_csv = yelp_df.to_csv()
     return yelp_csv
 
-
 def push_csv_to_bucket(csv, ACCESS_ID, ACCESS_KEY):
     s3_client = boto3.client('s3',
          aws_access_key_id=ACCESS_ID,
@@ -47,7 +43,6 @@ def push_csv_to_bucket(csv, ACCESS_ID, ACCESS_KEY):
 
     response =  s3_client.put_object(Body = csv, Bucket = 'yelp-data-engineering', Key = 'query_of_nyc_best')
     print(response)
-
 
 def main():
     load_dotenv(dotenv_path=find_dotenv(), verbose=True)
@@ -69,8 +64,5 @@ def main():
     yelp_csv = make_dataframe(queryresults)
     push_csv_to_bucket(yelp_csv, ACCESS_ID, ACCESS_KEY)
     
-
-
-
 if __name__ == '__main__':
     main()
